03eb5f25dfe016dbbed57a722d78bd887004c5e4
Walid Saleh <superwal@gmail.com>
2026-09-14 17:34:21 +0200

    Bind emoji picker to SUPER+. instead of SUPER+E

--- hypr/config/binds.lua
+++ hypr/config/binds.lua
@@ -71,7 +71,7 @@ hl.bind(mainMod .. " + code:86", function() zoomfunction(0.3) end, { repeating =
 hl.bind(mainMod .. " + Return",     hl.dsp.exec_cmd(launchPrefix .. TERMINAL))
 hl.bind(mainMod .. " + E",          hl.dsp.exec_cmd(launchPrefix .. FILE_MANAGER))
 hl.bind(mainMod .. " + T",          hl.dsp.exec_cmd(launchPrefix .. EDITOR))
-hl.bind(mainMod .. " + E",          hl.dsp.exec_cmd("wofi-emoji"))
+hl.bind(mainMod .. " + period",      hl.dsp.exec_cmd("wofi-emoji"))
 hl.bind(mainMod .. " + C",          hl.dsp.exec_cmd(launchPrefix .. CALCULATOR))
 hl.bind("XF86Calculator",           hl.dsp.exec_cmd(launchPrefix .. CALCULATOR))
 hl.bind(mainMod .. " + W",          hl.dsp.exec_cmd(launchPrefix .. BROWSER))
